7d624012c646eb3051c122a2aa08f094df2c5510
jimin <jimin00study@gmail.com>
2023-12-05 11:40:57 +0900

    공통의 동작을 함수로 정의

--- 04-hamburger-menu/js/script.js
+++ 04-hamburger-menu/js/script.js
@@ -18,16 +18,21 @@ $(function () {
     $dim.fadeToggle();
 
     // $menu가 보여지게 : isActive 조건에 따라서
-    if (isActive === false) {
+    if (!isActive) {
       //  $menu를 보이게
-      $menu.animate({ left: 0 }, 350);
-      // isActive를 반대로 설정
+      // $menu.animate({ left: 0 }, 350);
+      slideMenu(0);
       isActive = true;
     } else {
       //  $menu를 안 보이게
-      $menu.animate({ left: '-100%' }, 350);
-      // isActive를 반대로 설정
+      // $menu.animate({ left: '-100%' }, 350);
+      slideMenu('-100%');
       isActive = false;
     }
   });
+
+  // 공통의 동작을 함수로 정의
+  function slideMenu(pos) {
+    $menu.animate({ left: pos }, 350);
+  }
 });
